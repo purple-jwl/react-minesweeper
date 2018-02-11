7e2f45134b3f0096a6f178b228b600091f5f0a47
purple-jwl <purple.jwl@gmail.com>
2018-02-11 15:55:24 +0900

    state初期値をセットする

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
+import { getInitConfig } from './config';
 
-let store = createStore(reducers);
+let store = createStore(reducers, getInitConfig());
 
 render(
     <Provider store={store}>
