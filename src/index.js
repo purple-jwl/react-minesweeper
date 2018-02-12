3a9bcaba56e17b27efda8076cf83f7e79fbd36f1
purple-jwl <purple.jwl@gmail.com>
2018-02-12 15:37:54 +0900

    redux-thunkをstoreに適用する

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
 import { getInitConfig } from './config';
 
-let store = createStore(reducers, getInitConfig());
+let store = createStore(
+    reducers,
+    getInitConfig(),
+    applyMiddleware(thunk)
+);
 
 render(
     <Provider store={store}>
