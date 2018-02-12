b37884cf8bc0395ed14be6f5c39b066e815902a2
purple-jwl <purple.jwl@gmail.com>
2018-02-12 16:12:19 +0900

    Cellを開ける処理を仮実装

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,16 +7,111 @@ export const changeDifficulty = (difficulty) => {
     };
 };
 
-export const toggleFlag = (coordinate) => {
+export const toggleFlag = ({x, y}) => {
     return {
         type: 'TOGGLE_FLAG',
-        coordinate,
     };
 };
 
-export const openCell = (coordinate) => {
+export const openCell = ({x, y}) => {
+    return (dispatch, getState) => {
+        const currentState = getState();
+        const nextState = _openCell(x, y, currentState);
+        dispatch(setState(nextState));
+    }
+};
+
+export const setState = (state) => {
     return {
-        type: 'OPEN_CELL',
-        coordinate,
-    };
+        type: 'SET_STATE',
+        current_state: state,
+    }
+};
+
+const _openCell = (cx, cy, state) => {
+    if (state.isFirstOpen) {
+        state.isFirstOpen = false;
+        state.status = 'playing';
+
+        // this.startTimer();
+
+        Object.assign(state, _generateBoard(cx, cy, state));
+    }
+
+    // let remainingCells = state.remainingCells;
+    // const isOpened = state.isOpened.slice();
+    const queue = [[cx, cy]];
+
+    while (queue.length) {
+        const [cx, cy] = queue.shift();
+
+        if (state.isOpened[cy][cx] || state.isFlagged[cy][cx]) {
+            continue;
+        }
+
+        state.isOpened[cy][cx] = true;
+        state.remainingCells--;
+
+        if (state.board[cy][cx] === state.mineNumber) {
+            state.status = 'game over';
+            break;
+        }
+
+        if (state.remainingCells === state.mines) {
+            state.status = 'clear';
+            break;
+        }
+
+        if (state.board[cy][cx] !== 0) {
+            continue;
+        }
+
+        for (let y = -1; y <= 1; y++) {
+            for (let x = -1; x <= 1; x++) {
+                const ny = cy + y;
+                const nx = cx + x;
+                if (0 <= ny && ny < state.rows &&
+                    0 <= nx && nx < state.columns) {
+                    queue.push([nx, ny]);
+                }
+            }
+        }
+    }
+
+    return state;
+
+    //this.checkGameStatus(status);
+};
+
+const _generateBoard = (sx, sy, state) => {
+    const board = state.board.slice();
+
+    for (let m = 0; m < state.mines; m++) {
+        const my = Math.floor(Math.random() * state.rows);
+        const mx = Math.floor(Math.random() * state.columns);
+
+        if ((mx === sx && my === sy) || state.board[my][mx] === state.mineNumber) {
+            m--;
+            continue;
+        }
+
+        // 地雷を設置する
+        board[my][mx] = state.mineNumber;
+
+        // 周辺の地雷カウント
+        for (let y = -1; y <= 1; y++) {
+            for (let x = -1; x <= 1; x++) {
+                const ny = my + y;
+                const nx = mx + x;
+                if (0 <= ny && ny < state.rows &&
+                    0 <= nx && nx < state.columns &&
+                    board[ny][nx] !== state.mineNumber
+                ) {
+                    board[ny][nx]++;
+                }
+            }
+        }
+    }
+
+    return {board};
 };
