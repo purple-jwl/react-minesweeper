17d6a8173c1f8865fd785d4962486d4b18c6312d
purple-jwl <purple.jwl@gmail.com>
2018-02-14 10:38:39 +0900

    タイマーのactionとreducerを実装

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,10 @@ const reducers = (state = [], action) => {
             return Object.assign({}, state, action.initState);
         case 'SET_STATE':
             return Object.assign({}, state, action.state);
+        case 'CLEAR_TIMER':
+            return Object.assign({}, state, {seconds: 0});
+        case 'TICK_TIMER':
+            return Object.assign({}, state, {seconds: state.seconds + 1});
         default:
             return state;
     }
